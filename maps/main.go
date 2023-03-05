fa80c489a05f64c4a04121bc354709176127cecd
inosov <i.nosov@dyninno.com>
2023-03-05 17:31:55 +0200

    add func printMap

--- maps/main.go
+++ maps/main.go
@@ -16,5 +16,11 @@ func main() {
 	// colors[10] = "#ffffff"
 	// delete(colors, 10)
 
-	fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
