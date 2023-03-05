e1b003ad42c70813319fa02d22a4dffa7ed92e63
inosov <i.nosov@dyninno.com>
2023-03-05 18:11:14 +0200

    create interface

--- interfaces/main.go
+++ interfaces/main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import "fmt"
 
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+// func printGreeting(eb englishBot) {
+// 	fmt.Println(eb.getGreeting())
+// }
+
+// func printGreeting(sb spanishBot) {
+// 	fmt.Println(sb.getGreeting())
+// }
+
+func (englishBot) getGreeting() string {
+	// VERY custom logic for generating an english greeting
+	return "Hello there!"
+}
+
+func (spanishBot) getGreeting() string {
+	// VERY custom logic for generating an spanish greeting
+	return "Hola!"
 }
